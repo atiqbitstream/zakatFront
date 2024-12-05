68363e473c9c52b34d3530acda5f443cc3c61e3d
atiqbitstream <atiq.bitstream@gmail.com>
2024-12-05 13:01:25 +0500

    added simple 'login' functionality

--- src/app/features/auth/auth.component.ts
+++ src/app/features/auth/auth.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
+import { LoginRequest } from './interfaces/loginRequest.interface';
 
 @Component({
   selector: 'app-auth',
@@ -7,5 +10,46 @@ import { Component } from '@angular/core';
   styleUrl: './auth.component.css'
 })
 export class AuthComponent {
+  constructor(private authService: AuthService, private router:Router) {}
+
+  loginForm!: FormGroup;
+
+  ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      email: new FormControl(''),
+      password: new FormControl(''),
+    });
+  }
+
+  get username() {
+    return this.loginForm.get('username');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
+
+  get email()
+  {
+    return this.loginForm.get('email')
+  }
+
+  onUserLogin() {
+    if (this.loginForm.valid) {
+      const loginRequest: LoginRequest = this.loginForm.value;
+
+      this.authService.loginUser(loginRequest).subscribe({
+        next: (response) => {
+          console.log(response);
+          this.router.navigate(['/home'])
+        },
+        error: (err) => {
+          console.log('Login failed', err);
+        },
+      });
+    } else {
+      console.log('Form is invalid');
+    }
+  }
 
 }
---
+++ src/app/features/auth/interfaces/loginResponse.interface.ts
@@ -0,0 +1,6 @@
+export interface LoginReponse
+{
+    accessToken :string;
+    refreshToken:string;
+    user:string;
+}
